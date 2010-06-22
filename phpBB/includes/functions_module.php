c18d0f7f0decadcbdb7e279174d2ebc38cbeeb7a
Cullen Walsh <ckwalsh@phpbb.com>
2010-06-22 14:13:10 -0700

    [ticket/9670] Remove use of eval() from includes/functions_module.php using a custom parser
    This patch also introduces a system where modules may implement their auth logic in php. Implementation of this across modules should provide better performance than the parser.  Note, this code is not a bottleneck point.

--- phpBB/includes/functions_module.php
+++ phpBB/includes/functions_module.php
@@ -315,6 +315,11 @@ class p_master
 	{
 		global $auth, $config;
 
+		if (method_exists($this, 'module_auth_local'))
+		{
+			return $this->module_auth_local($forum_id);
+		}
+
 		$module_auth = trim($module_auth);
 
 		// Generally allowed to access module if module_auth is empty
@@ -361,7 +366,11 @@ class p_master
 		$forum_id = ($forum_id === false) ? $this->acl_forum_id : $forum_id;
 
 		$is_auth = false;
-		eval('$is_auth = (int) (' . preg_replace(array('#acl_([a-z0-9_]+)(,\$id)?#', '#\$id#', '#aclf_([a-z0-9_]+)#', '#cfg_([a-z0-9_]+)#', '#request_([a-zA-Z0-9_]+)#'), array('(int) $auth->acl_get(\'\\1\'\\2)', '(int) $forum_id', '(int) $auth->acl_getf_global(\'\\1\')', '(int) $config[\'\\1\']', '!empty($_REQUEST[\'\\1\'])'), $module_auth) . ');');
+		
+		static $parser = false;
+		if (empty($parser)) $parser = new ModuleAuthParser();
+		$parser->forum_id = $forum_id;
+		$is_auth = $parser->parse($module_auth);
 
 		return $is_auth;
 	}
@@ -877,6 +886,146 @@ class p_master
 				$user->add_lang($add_files);
 			}
 		}
+	}
+}
+
+/**
+ * Used to parse module auth strings in the database.
+ * This is probably slower than eval(), but makes eval() unecessary.
+ *
+ * @author APTX
+ **/
+class ModuleAuthParser
+{
+	
+	var $_tokens;
+	var $_c;
+	var $_i;
+	var $forum_id;
+	
+	/**
+	 * Parses the expression and returns the result.
+	 * Calls _variable_callback for variable values.
+	 * 
+	 * @param $string string with epxression to parse
+	 * 
+	 * @return bool Expression result
+	 */
+	function parse($string)
+	{
+		$this->_tokens = array();
+		$this->_i = 0;
+		
+		preg_match_all('~\$id|&&|\|\||\(|\)|!|[a-z][a-zA-Z0-9_]*(?:,\$id)?~', $string, $this->_tokens);
+		$this->_tokens = $this->_tokens[0];
+		$this->_c = &$this->_tokens[0];
+		
+		return $this->_expr(false);
+	}
+
+	/**
+	 * Returns the value of variable $var_name
+	 * @param $var_nbmae variable name
+	 * 
+	 * @return bool variable value
+	 */
+	function _variable_callback($var_name)
+	{
+		global $auth, $config;
+		$prefix = substr($var_name, 0, 4);
+		
+		switch ($prefix)
+		{
+			case '$id':
+				return $this->forum_id;
+			
+			case 'acl_':
+				$parts = array();
+				preg_match('#acl_([a-z0-9_]+)(,\$id)?#', $var_name, $parts);
+				
+				if (empty($parts[2]))
+				{
+					return $auth->acl_get($parts[1]);
+				}
+				else
+				{
+					return $auth->acl_get($parts[1], $this->forum_id);
+				}
+
+			case 'aclf':
+				return $auth->acl_getf_global(substr($var_name, 5));
+
+			case 'cfg_':
+				return $config[substr($var_name, 4)];
+
+			case 'requ':
+				return !empty($_REQUEST[substr($var_name, 8)]);
+			
+			default:
+				return false;
+		}
+	}
+
+	function _expr($get)
+	{
+		$left = $this->_prim($get);
+		while (true)
+		{
+			switch ($this->_c)
+			{
+				case '&&':
+					$right = $this->_prim(true);
+					$left = $left && $right;
+				break;
+				case '||':
+					$right = $this->_prim(true);
+					$left = $left || $right;
+				break;
+				default:
+					return $left;
+			}
+		}
+	}
+	
+	function _prim($get)
+	{
+		if ($get) $this->_get_token();
+
+		switch ($this->_c)
+		{
+			case '$id':
+				$ret = $this->_variable_callback('$id');
+				$this->_get_token();
+				return $ret;
+			break;
+			case '!':
+				return !$this->_prim(true);
+			break;
+			case '(':
+				$e = $this->_expr(true);
+				if ($this->_c != ')')
+				{
+					print 'missing closing )';
+					return 0;
+				}
+				$this->_get_token();
+				return $e;
+			break;
+			case '':
+				return false;
+			default:
+				$ret = $this->_variable_callback($this->_c);
+				$this->_get_token();
+				return $ret;
+		}
+	}
+	
+	function _get_token()
+	{
+		++$this->_i;
+		if ($this->_i > sizeof($this->_tokens))
+			return '';
+		$this->_c = &$this->_tokens[$this->_i];
 	}
 }
 
